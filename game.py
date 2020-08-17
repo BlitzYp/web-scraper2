4c2a68d27d1b9daadb3c984d74eebd6b8c25419e
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2020-08-17 20:10:20 +0300

    Added color

--- game.py
+++ game.py
@@ -1,18 +1,21 @@
 import random
+import termcolor
 
 
 def handle_game(data: tuple, date: str, location: str):
-    print("Hello there! Today I will test you knowledge about quotes!")
+    print(termcolor.colored(
+        "Hello there! Today I will test you knowledge about quotes!", 'green'))
     tries: int = 5
     while tries > 0:
-        print(data[0])
+        print(termcolor.colored(data[0], 'yellow'))
         user_res: str = str(input("Who is the author of this quote?: "))
         if user_res.lower() in data[1].lower():
-            return f'You are correct! The author is {data[1]}'
+            return termcolor.colored(f'You are correct! The author is {data[1]}', 'green')
         else:
             tries -= 1
-            print(f'{user_res}: is not correct! Try again!')
-            print("Hint")
-            print(random.choice([date, location]))
+            print(termcolor.colored(
+                f'{user_res}: is not correct! Try again!', 'red'))
+            print("Hint: ", termcolor.colored(
+                random.choice([date, location]), 'green'))
             continue
-    return f'Ahhhh the answer was: {data[1]}...Do you want to play again?: '
+    return termcolor.colored(f'Ahhhh the answer was: {data[1]}......', 'red')
